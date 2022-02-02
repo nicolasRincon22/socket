aa6e549bcdc1181decac6d7667ec9a7095a41007
nicolasRincon22 <nicolas.rinconmarin@alessandrinimainardi.edu.it>
2022-02-02 15:04:19 +0100

    socket

--- client.py
+++ client.py
@@ -5,17 +5,18 @@ HOST="127.0.0.1"
 PORT=65432
 
 with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
-   s.connect((HOST,PORT))
-   while True: 
-       primoNumero=input("Inserisci il primo numero. exit() per uscire")
-       if primoNumero=="exit()":
-           break
-       primoNumero=float(primoNumero)
-       operazione=input("Inserisci l'operazione (+,-,*,/,%)")
-       secondoNumero=float(input("Inserisci il secondo numero"))
-       messaggio={'primoNumero': primoNumero,'operazione':operazione,'secondoNumero':secondoNumero}
-       # Transformiammo l'oggetto in una stringa 
-       messaggio=json.dumps(messaggio) 
-       s.sendall(messaggio.encode("UTF-8"))
-       data=s.recv(1024)#aspettiamo che il server ci mandi delle domande 
-       print("Risultato: ",data.decode())
+    s.connect((HOST,PORT))
+    while True:
+        primoNumero=input("Inserisci primo numero. exit() per uscire")
+        if primoNumero=="exit()":
+            break
+        primoNumero=float(primoNumero)
+        operazione=input("Inserisci l'operazione (+,-,*,/,%)")
+        secondoNumero=float(input("Inserisci il secondo numero"))
+        messaggio={'primoNumero':primoNumero,
+        'operazione':operazione,
+        'secondoNumero':secondoNumero}
+        messaggio=json.dumps(messaggio)#Trasformiamo l'oggetto che viene scritto  in una stringa
+        s.sendall(messaggio.encode("UTF-8"))
+        data=s.recv(1024)
+        print("Risultato: ",data.decode())
